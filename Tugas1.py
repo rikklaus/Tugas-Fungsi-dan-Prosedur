29e3309ef358efe826058cab5a01318678d4638c
rikklaus <erikklaus27@gmail.com>
2024-11-05 23:24:07 +0700

    Versi 1.1

--- Tugas1.py
+++ Tugas1.py
@@ -1,1 +1,22 @@
-print("hellow world")
+# Nama: Erik Klaus Keifas Kalalo
+# Kelas: 1A
+# NIM: 2400887
+
+def fibonacci (n):
+    a = 0
+    b = 1
+    
+    if n == 1:
+        print (a)
+    else:
+        print (a)
+        print (b)
+    
+    for p in range (2,n):
+        c = a+b
+        a = b
+        b = c
+        print(c)
+
+i = int(input("Masukkan angka: "))
+print(fibonacci(i))
---
+++ Tugas3.py
@@ -0,0 +1,3 @@
+# Nama: Erik Klaus Keifas Kalalo
+# Kelas: 1A
+# NIM: 2400887
