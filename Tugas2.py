a85b12462c45b2d19caa765910fb8548ee0b9e40
rikklaus <erikklaus27@gmail.com>
2024-11-06 01:04:49 +0700

    Tugas Clear

--- Tugas2.py
+++ Tugas2.py
@@ -2,19 +2,34 @@
 # Kelas: 1A
 # NIM: 2400887
 
-def login ():
+def cek_username(username):
+    return username == "Daspro2023"
+
+def cek_password(password):
+    return password == "latihan"
+
+def tampilkan_pesan_berhasil():
+    print("Selamat datang di aplikasi Dasar Pemrograman")
+
+def tampilkan_pesan_gagal(kesempatan):
+    print(f"Login salah! Kesempatan anda {kesempatan} kali lagi")
+
+def tampilkan_pesan_akses_ditolak():
+    print("Anda tidak diperkenankan mengakses aplikasi ini")
+
+def login():
     print("Silakan Login")
     
-    for kesempatan in range (3,0,-1):
-        username = (str(input("Username:    ")))
-        password = (str(input("Password:    ")))
-        if password == "latihan":
-            print("Selamat datang di aplikasi Dasar Pemrograman")
+    for kesempatan in range(3, 0, -1):
+        username = input("Username: ")
+        password = input("Password: ")
+        
+        if cek_username(username) and cek_password(password):
+            tampilkan_pesan_berhasil()
             break
         else:
-            print(f"Login salah! kesempatan anda {kesempatan - 1} kali lagi")
-    
+            tampilkan_pesan_gagal(kesempatan - 1)
     else:
-        print("Anda tidak diperkenankan mengakses aplikasi ini")
-    
+        tampilkan_pesan_akses_ditolak()
+
 login()
--- Tugas3.py
+++ Tugas3.py
@@ -2,3 +2,29 @@
 # Kelas: 1A
 # NIM: 2400887
 
+def konversi_waktu(total_waktu):
+        jam = total_waktu // 3600
+        sisa = total_waktu % 3600
+        menit = sisa // 60
+        detik = sisa % 60
+        return jam, menit, detik
+
+def hitungwaktu ():
+        print("Input waktu mulai:    ")
+        jam_mulai = int(input("Jam:     "))
+        menit_mulai = int(input("Menit:     "))
+        detik_mulai = int(input("detik:     "))
+        
+        print("Input waktu selesai:    ")
+        jam_selesai = int(input("Jam:     "))
+        menit_selesai = int(input("Menit:     "))
+        detik_selesai = int(input("Detik:     "))
+        
+        total_mulai = jam_mulai * 3600 + menit_mulai * 60 + detik_mulai
+        total_selesai = jam_selesai * 3600 + menit_selesai * 60 + detik_selesai
+        total_waktu = total_selesai - total_mulai
+        
+        hasiljam, hasilmenit, hasildetik = konversi_waktu(total_waktu)
+        print(f"{hasiljam} Jam - {hasilmenit} Menit - {hasildetik} Detik")
+        
+hitungwaktu()
