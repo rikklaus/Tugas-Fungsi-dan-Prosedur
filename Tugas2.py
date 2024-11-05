73c249df0e6c9da1d10e89a51bb9f0a14a997f2e
rikklaus <erikklaus27@gmail.com>
2024-11-05 23:46:33 +0700

    Versi 1.3

--- Tugas2.py
+++ Tugas2.py
@@ -2,4 +2,19 @@
 # Kelas: 1A
 # NIM: 2400887
 
-
+def login ():
+    print("Silakan Login")
+    
+    for kesempatan in range (3,0,-1):
+        username = (str(input("Username:    ")))
+        password = (str(input("Password:    ")))
+        if password == "latihan":
+            print("Selamat datang di aplikasi Dasar Pemrograman")
+            break
+        else:
+            print(f"Login salah! kesempatan anda {kesempatan - 1} kali lagi")
+    
+    else:
+        print("Anda tidak diperkenankan mengakses aplikasi ini")
+    
+login()
